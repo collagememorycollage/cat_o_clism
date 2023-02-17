219cd28f7c669469ff78a47337205d659ab62cab
Alex <collagememorycollage@gmail.com>
2023-02-18 01:03:58 +0300

    добавил еще пак с текстурами

--- TilesetElement.tsx
+++ TilesetElement.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="TilesetElement" tilewidth="12" tileheight="12" tilecount="1960" columns="49">
- <image source="../TilesetElement.png" width="597" height="484"/>
+<tileset version="1.9" tiledversion="1.9.2" name="TilesetElement" tilewidth="12" tileheight="12" tilecount="1960" columns="31">
+ <image source="../TilesetElement.png" width="379" height="320"/>
 </tileset>
--- острова и плиты.tsx
+++ острова и плиты.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="острова и плиты" tilewidth="12" tileheight="12" tilecount="4293" columns="81">
- <image source="assets/острова и плиты.png" width="974" height="647"/>
+ <image source="assets/PNG/острова и плиты.png" width="974" height="647"/>
 </tileset>
